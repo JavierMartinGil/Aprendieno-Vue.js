f18d2809a245b49a9555c4c917c403d97bdf5235
Javier Martin Gil <javiermartingil83@gmail.com>
2019-10-21 11:53:36 +0200

    Inicio proyecto 1

---
+++ proyecto1/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" class="container">
+    <Cabecera></Cabecera>
+    <Lista />
+  </div>
+</template>
+
+<script>
+import Cabecera from "./components/Cabecera.vue";
+import Lista from "./components/Lista";
+
+export default {
+  name: "app",
+  components: {
+    Cabecera,
+    Lista
+  }
+};
+</script>
---
+++ proyecto1/src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    frutas: [
+      { nombre: 'Manzana', cantidad: 0 },
+      { nombre: 'Pera', cantidad: 1 },
+      { nombre: 'Melón', cantidad: 4 },
+    ]
+  },
+  mutations: {
+    aumentar(state, index) {
+      state.frutas[index].cantidad++
+    },
+
+    reiniciar(state) {
+      state.frutas.forEach(elemento => {
+        elemento.cantidad = 0;
+      });
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
