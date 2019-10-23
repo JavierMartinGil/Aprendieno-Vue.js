a9f3f53cf9e7cc6583942f5ebd21ac3a4930f39c
Javier Martin Gil <javiermartingil83@gmail.com>
2019-10-23 12:49:24 +0200

    Routing Vue

---
+++ vue-route/src/views/Fotos.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Galería de fotos</h1>
+    <router-link
+      :to="{name: 'fotos', params:{id: item}}"
+      v-for="(item,index) of fotosArray"
+      :key="index"
+    >
+      <button>Foto {{item}}</button>
+    </router-link>
+    <Foto />
+    <button @click="home()">Inicio</button>
+    <button @click="anterior()">Anterior</button>
+    <button @click="siguiente()">Siguiente</button>
+  </div>
+</template>
+
+<script>
+import Foto from "@/components/Fotografia.vue";
+
+export default {
+  components: {
+    Foto
+  },
+  data() {
+    return {
+      fotosArray: [1, 2, 3]
+    };
+  },
+
+  methods: {
+    home() {
+      this.$router.push("/");
+    },
+    anterior() {
+      this.$router.go(-1);
+    },
+    siguiente() {
+      this.$router.go(1);
+    }
+  }
+};
+</script>
